0eb2afcd2ee5d69db75d571a4c82ddc7747e81f8
Erwin Jimenez <erwinjv98@gmail.com>
2021-12-28 16:16:20 -0300

    Variables de entorno de DB actualizado

--- config/database.js
+++ config/database.js
@@ -1,13 +1,24 @@
+
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'whishlist-api'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'er05win98'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: config.host || "127.0.0.1" ,
+      port: config.port ||  5432,
+      database: config.database || 'whishlist-api',
+      user: config.user || 'postgres',
+      password: config.password ||  'er05win98',
+      ssl: {
+        rejectUnauthorized: false
+      },
+      options: {
+        ssl: config.host ? true : false,
+      },
     },
+    debug: false,
   },
 });
+ 
